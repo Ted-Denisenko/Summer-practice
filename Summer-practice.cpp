b2dda9cb225a2cafed471a91dd588480bb3b58ea
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-10 16:35:51 +0300

    Добавлен вывод исходных и результирующего списков на экран Проведена работа с комментариями Убраны неиспользующиеся библиотеки

--- Summer-practice.cpp
+++ Summer-practice.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <list>
-#include <cstdio>
-#include <cassert>
-#include <algorithm>
 
 
 // Задача 5
@@ -15,16 +11,10 @@
 Вывести на экран списки С1, С2, С3.
 */
 
-/*
-В файле идут значения в строке, в качестве разделителя используется отрицательное число.
-Числа до отр. числа – список С1, числа после отр. числа – список С2.
-Собрать два списка в один, отсортировать с помощью сортировки слиянием
-Вывести списки С1, С2 и отсортированный С3.
-*/
-
 
 int main()
 {
+    // читаем данные из исходного файла
     std::ifstream fin;
     fin.open("C:\\Code\\summer_practice.txt");
     int read_number;
@@ -32,7 +22,7 @@ int main()
     std::list<int> C1;
     std::list<int> C2;
 
-    // читаем файл и разбиваем на два списка
+    // заполняем списки наборами из положительных значений
     while (fin >> read_number)
     {
         if (read_number < 0)
@@ -46,14 +36,32 @@ int main()
             C2.push_back(read_number);
     }
 
-    // заполняем список С3 неотсортированными значениями из C1 и C2
+    // соединяем списки С1 и С2 в список С3 и сортируем значения по возрастанию
     std::list<int> C3;
     std::list<int>::iterator i = C3.begin();
     C3.insert(i, C1.begin(), C1.end());
     i = C3.end();
     C3.insert(i, C2.begin(), C2.end());
-    // сортируем С3
     C3.sort();
 
-    fin.close();     // закрываем файл
+    // выводим значения списков на экран
+    std::cout << "C1 and C2 values before merging in C3" << std::endl;
+    std::cout << "\nC1:   ";
+    for (std::list<int>::iterator i = C1.begin(); i != C1.end(); ++i)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << "\nC2:   ";
+    for (std::list<int>::iterator i = C2.begin(); i != C2.end(); ++i)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << "\n\nC3 list sorted values\n" << std::endl;
+    for (std::list<int>::iterator i = C3.begin(); i != C3.end(); ++i)
+    {
+        std::cout << *i << " ";
+    }
+
+    // закрываем файл
+    fin.close();
 }
